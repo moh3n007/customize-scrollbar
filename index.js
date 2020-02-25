6e445a13c0eca30b1bd0a9cebbcb980c0174d439
mohsen007 <lotfimohsen007@gmail.com>
2020-02-25 18:18:15 +0330

    add events to scrollbar for scrolling page

--- index.js
+++ index.js
@@ -6,20 +6,34 @@ window.addEventListener('load' , () => {
                 scrollBtn.addEventListener('mousedown' , (event) => {
                     let scrlBtn = event.target;
                     let scrlBtnHeight = scrlBtn.clientHeight;
+                    let viewPortHeight = document.body.clientHeight;
+                    let bodyHeight = document.documentElement.scrollHeight;
+                    let scrlMoveWrap = viewPortHeight - scrlBtnHeight;
+                    let pageScrlTop = document.documentElement.scrollTop;
                     let oldMouseY = event.clientY;
                     let preScrlTop = parseInt(scrlBtn.style.top);
-                    // console.log(scrlBtnHeight);
                     document.body.onmousemove = (event) => {
                         let newMouseY = event.clientY;
-                        scrollBtn.style.top = `${preScrlTop + newMouseY - oldMouseY}px`;
-                        if(parseInt(scrollBtn.style.top) < 0) {
-                            scrollBtn.style.top = 0;
-                        } else if(parseInt(scrollBtn.style.top) > (document.body.clientHeight - scrlBtnHeight)) {
-                            scrollBtn.style.top = `${document.body.clientHeight - scrlBtnHeight}px`;
+                        console.log((bodyHeight - viewPortHeight) , scrlMoveWrap);
+                        scrlBtn.style.top = `${preScrlTop + (((newMouseY - oldMouseY)/viewPortHeight)*100)}%`;
+                        if(parseInt(scrlBtn.style.top)*100 < 0.001) {
+                            scrlBtn.style.top = 0;
+                        } else if(parseInt(scrlBtn.style.top) > (scrlMoveWrap/viewPortHeight)*100 - 0.00001) {
+                            scrlBtn.style.top = `${(scrlMoveWrap/viewPortHeight)*100}%`;
                         }
-                        // console.log(oldMouseY , newMouseY);
+                        
+                        if(((bodyHeight-viewPortHeight)/scrlMoveWrap)*(newMouseY - oldMouseY) > bodyHeight-viewPortHeight) {
+                            window.scrollTo(0 , Math.round(bodyHeight-viewPortHeight));
+                        } else {
+                            if((bodyHeight - viewPortHeight) > scrlMoveWrap) {
+                                console.log('bozorgtar',pageScrlTop,scrlMoveWrap)
+                                window.scrollTo(0 , Math.round(pageScrlTop + ((bodyHeight-viewPortHeight)/scrlMoveWrap)*(newMouseY - oldMouseY)));
+                            } else {
+                                console.log('kochictar',pageScrlTop,scrlMoveWrap,bodyHeight , viewPortHeight,newMouseY - oldMouseY)
+                                window.scrollTo(0 , Math.round(pageScrlTop + ((bodyHeight-viewPortHeight)/scrlMoveWrap)*(newMouseY - oldMouseY)));
+                            }
+                        }
                     }
-
                 });  
                 scrollBtn.addEventListener('mouseup' , () => {
                     document.body.onmousemove = null;
@@ -34,4 +48,8 @@ window.addEventListener('load' , () => {
 });
 window.addEventListener('mouseup' , () => {
     document.body.onmousemove = null;
+});
+window.addEventListener('scroll' , () => {
+    let scrollbar = document.getElementById('scrollBar');
+    scrollbar.style.top = document.documentElement.scrollTop + 'px'; 
 })
